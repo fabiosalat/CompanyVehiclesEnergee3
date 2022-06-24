fd4aac0692e143f052e6af250961f4d835220bce
Fabio Salatino <93608879+fabiosalat@users.noreply.github.com>
2022-06-24 10:51:30 +0200

    first commit

---
+++ src/main/java/com/energee3/stage/companyVehicles/CompanyVehiclesApplication.java
@@ -0,0 +1,13 @@
+package com.energee3.stage.companyVehicles;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CompanyVehiclesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CompanyVehiclesApplication.class, args);
+	}
+
+}
